cc7580defb01b70038209e433530687cb109d1ba
Sheik-0706 <sheiksybudeen7@gmail.com>
2025-01-10 13:09:40 +0530

    update1

--- src/test/java/orgtest/Git.java
+++ src/test/java/orgtest/Git.java
@@ -3,7 +3,7 @@ package orgtest;
 public class Git {
 	
 	public static void main(String[] args) {
-		System.out.println("hello");
+		System.out.println("Welcome");
 		System.out.println("Hi");
 	}
 
